1c2cc792ab07ae6167311a3bb9f785c2221fc928
scartissue1 <82233682+scartissue1@users.noreply.github.com>
2023-09-24 18:29:29 +0300

    Update task1.c

--- first_lab/task1.c
+++ first_lab/task1.c
@@ -4,106 +4,246 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
-void multiple_by (int number) {
-    int to_hundred = 0;
-    int flag = 0;
+enum multiple_by_status_codes {
+    mbsc_ok,
+    mbsc_no_number_found,
+    mbsc_overflow,
+    mbsc_underflow
+};
 
-    while (to_hundred < 100) {
+enum multiple_by_status_codes multiple_by (int number, int * result, int result_length) {
+    int to_hundred = number;
+    int flag = 0, i = 0;
+
+    if (number < 0) return mbsc_underflow;
+    while (to_hundred <= 100) {
+        if (i == result_length) return mbsc_overflow;
+        result[i] = to_hundred;
+        i++;
         to_hundred += number;
-        if (to_hundred <= 100) {
-            printf("%d ", to_hundred);
-            flag = 1;
-        }
+        flag = 1;
     }
 
-    if (!flag) printf("No number in [1, 100] can be multiplied by %d", number);
-    printf("\n");
+    if (!flag) return mbsc_no_number_found;
+    else return mbsc_ok;
 }
 
-bool is_prime (int number) {
+enum is_prime_status_codes {
+    ipsc_one,
+    ipsc_underflow,
+    ipsc_true,
+    ipsc_false
+};
+
+enum is_prime_status_codes is_prime (int number) {
+    if (number <= 0) return ipsc_underflow;
+    if (number == 1) return ipsc_one;
+
     int prime_checker = 2;
-    while (prime_checker < number / 2 + 1) {
-        if (number % prime_checker == 0) return false;
+    while (prime_checker < (int)sqrt(number) + 1) {
+        if (number % prime_checker == 0 && is_prime(prime_checker)) {
+            return ipsc_false;
+        }
         prime_checker++;
     }
-    return true;
+    return ipsc_true;
 }
 
-void number_split(int number) {
+int digits_count (int number) {
     int digits_count = 0, check = number;
     while (check != 0) { 
         digits_count++;
         check /= 10;
     }
-    
-    while (digits_count) {
+    return digits_count;
+}
+enum number_split_status_codes {
+    nssc_ok,
+    nssc_overflow,
+    nssc_invalid_parameter
+};
+
+enum number_split_status_codes number_split(int number, int * result, int digits) {
+    if (number < 0) return nssc_invalid_parameter;
+    int index = 0;
+    while (digits) {
         int splitter = 10, splitter_power = 1;
-        while (splitter_power < digits_count) {
+        while (splitter_power < digits) {
             splitter *= 10;
             splitter_power++;
         }
-        printf("%d ", number % splitter / (splitter / 10));
-        digits_count--;
-
+        if (index == digits) return nssc_overflow;
+        result[index] = number % splitter / (splitter / 10);
+        index++;
+        digits--;
     }
-    printf("\n");
+    return nssc_ok;
 }
 
-void power_table(int number) {
+enum power_table_status_codes {
+    ptsc_ok,
+    ptsc_invalid_parameter,
+    ptsc_overflow
+};
+
+enum power_table_status_codes power_table(int number, int ** result) {
     if (1 <= number && number <= 10) {
-        printf("base | powers from 1 to %d\n", number);
         for (int base = 1; base < 11; base++) {
-            printf("%d |", base);
             int start_base = base;
             for (int power = 1; power < number; power++) {
                 start_base *= base;
-                printf("%d | ", start_base);
+                if (base - 1< 10 && power - 1 < number) result[base - 1][power - 1] = start_base;
+                else return ptsc_overflow;
             }
-            printf("\n");
         }
+        return ptsc_ok;
     }
-    else printf("Number have to be in [1, 10]\n");
+    else return ptsc_invalid_parameter;
 }
 
-long long int sum_to_the_number (int number) {
-    long long int sum = 0;
-    int curr = 1;
-    while (curr <= number) {
-        sum += curr;
-        curr++;
-    }
-    return sum;
+enum sum_to_the_number_status_codes {
+    sttnsc_ok,
+    sttnsc_invalid_parameter
+};
+
+enum sum_to_the_number_status_codes sum_to_the_number (int number, long long int * result) {
+    if (number < 0) return sttnsc_invalid_parameter;
+    
+    *result = ((1 + number) / 2) * number;
+    return sttnsc_ok;
 }
 
-long long int factorial (int number) {
-    if (number == 0) return 1;
-    else if (number > 0) return number * factorial(number - 1);
-    else return -1;
+enum factorial_status_codes {
+    fsc_ok,
+    fsc_invalid_parameter
+};
+
+enum factorial_status_codes factorial (int number, long long int * result) {
+    if (number == 0 || number == 1) { 
+        *result = 1;
+        return fsc_ok;
+    }
+    else if (number > 1) {
+        enum factorial_status_codes recursive_status_code;
+        recursive_status_code = factorial(number - 1, result);
+        if (recursive_status_code == fsc_ok) *result *= number;
+        return recursive_status_code;
+    }
+    else return fsc_invalid_parameter;
+}
+
+bool is_num (char* string) {
+    for (int i = 0; i < strlen(string); i++) {
+        if (!isdigit(string[i])) return false;
+    }
+    return true;
 }
 
 int main(int argc, char* argv[]) {
     int validation_flag = 1;
-    if (argc == 3 && strlen(argv[2])) {
+    if (argc == 3 && is_num(argv[1])) {
         int number = atoi(argv[1]);
-        if (number) {
-            if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'h') multiple_by(number);
-
-            else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'p') {
-                if (is_prime(number)) printf("%d is prime \n", number);
-                else printf("%d is not prime \n", number);
+        if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'h')  { 
+            int result_length = 100 / number;
+            int * multipliers_result = (int*)malloc(sizeof(int) * result_length);                
+            switch (multiple_by(number, multipliers_result, result_length)) {
+                case mbsc_ok: 
+                    for (int i = 0; i < result_length; i++) {
+                        printf("%d ", multipliers_result[i]);
+                    }
+                    printf("\n");
+                    break;
+                case mbsc_no_number_found:
+                    printf("No number in [1, 100] can be multiplied by %d\n", number);
+                    break;
+                case mbsc_overflow:
+                    printf("%d is not enough to contain multipliers of %d\n", result_length, number);
+                    break;
+                case mbsc_underflow:
+                    printf("Number must be positive\n");
+                    break;
             }
-
-            else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 's') number_split(number);
-
-            else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'e') power_table(number);
-
-            else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'a') printf("%lld\n", sum_to_the_number(number));
-
-            else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'f') printf("%lld\n", factorial(number));
-            else validation_flag = 0;
+            
+            }    
+        else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'p') {
+            switch (is_prime(number)) {
+                case ipsc_true: printf("%d is prime\n", number); break;
+                case ipsc_false: printf("%d is not prime\n", number); break;
+                case ipsc_underflow: printf("Number have to be positive\n"); break;
+                case ipsc_one: printf("1 is prime and not prime\n"); break;
+                    
+            }
         }
+
+        else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 's') {
+            int d_count = digits_count(number);
+            int * splitter_result = (int*)malloc(sizeof(int) * d_count);
+            switch(number_split(number, splitter_result, d_count)) { 
+                case nssc_ok:
+                    for (int i = 0; i < d_count; i++) {
+                        printf("%d ", splitter_result[i]);
+                    }
+                    printf("\n");
+                    break;
+                case nssc_overflow:
+                    printf("%d is not enough to contain digits of %d\n", d_count, number);
+                    break;
+                case nssc_invalid_parameter:
+                    printf("Invalid parameter\n");
+                    break;
+            }
+        }
+        else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'e')  {
+            int ** power_table_result = (int**)malloc(sizeof(int*) * 10);
+            for (int i = 0; i < 10; i++) {
+                power_table_result[i] = (int*)malloc(sizeof(int) * number);
+            }
+            switch (power_table(number, power_table_result)) {
+                case ptsc_ok:
+                    for (int i = 1; i <= 10; i++) {
+                        printf("%d | ", i);
+                        for (int j = 1; j < number; j++) {
+                            printf("%d| ", power_table_result[i - 1][j - 1]);
+                        }
+                        printf("\n");
+                    }
+                    break;
+                case ptsc_invalid_parameter:
+                    printf("Invalid parameter\n");
+                    break;
+                case ptsc_overflow:
+                    printf("Overflow\n");
+                    break;
+            }
+        }
+        else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'a') {
+            long long int sum_result = 0;
+            switch(sum_to_the_number(number, &sum_result)) {
+                case sttnsc_ok:
+                    printf("%lld\n", sum_result);
+                    break;
+                case sttnsc_invalid_parameter:
+                    printf("Invalid parameter\n");
+                    break;
+            }
+        }
+
+        else if ((argv[2][0] == '-' || argv[2][0] == '/') && argv[2][1] == 'f') {
+            long long int factorial_result;
+            switch(factorial(number, &factorial_result)) {
+                case fsc_ok:
+                    printf("%lld\n", factorial_result);
+                    break;
+                case fsc_invalid_parameter:
+                    printf("Invalid parameter\n");
+                    break;
+            }
+        }
+            
         else validation_flag = 0;
+        
     }
     else validation_flag = 0;
     if (!validation_flag) printf("Wrong flag!\n");
