df74021792c2e2968bc5baccb7ae1df6501167db
scartissue1 <82233682+scartissue1@users.noreply.github.com>
2023-10-20 04:03:45 +0300

    Update task9.c

--- second_lab/task9.c
+++ second_lab/task9.c
@@ -4,15 +4,13 @@
 #include <math.h>
 #include <ctype.h>
 
-#define EPS 1e-10
-
 size_t _strlen(char * string) {
     size_t size = 0;
     while (string[size++] != '\0');
     return size - 1;
 }
 
-int double_length(double number) {
+int double_length(double number, const double EPS) {
     double tmp = number;
     int number_size = 0;
     while (fabs((int)tmp - tmp) > EPS) {
@@ -40,8 +38,8 @@ int greatest_common_divisor(int first, int second) {
     return second ? greatest_common_divisor(second, first % second) : first;
 }
 
-void common_fraction(double number, int * numerator, int * denominator) {
-    int size = double_length(number);
+void common_fraction(double number, int * numerator, int * denominator, const double EPS) {
+    int size = double_length(number, EPS);
     int power = 1;
     for (int i = 0; i < size; i++) {
         power *= 10;
@@ -53,7 +51,7 @@ void common_fraction(double number, int * numerator, int * denominator) {
     *denominator /= gsc;
 }
 
-double * final_representation_in_base (size_t * result_size, const int base, int quantity, ...) {
+double * final_representation_in_base (const double EPS, size_t * result_size, const int base, int quantity, ...) {
     va_list fractions;
     va_start(fractions, quantity);
     *result_size = 0;
@@ -63,7 +61,7 @@ double * final_representation_in_base (size_t * result_size, const int base, int
         int denominator = 1;
         double dnumber = va_arg(fractions, double);
         if (dnumber > 1 - EPS || dnumber < EPS) return NULL;
-        common_fraction(dnumber, &numerator, &denominator);
+        common_fraction(dnumber, &numerator, &denominator, EPS);
         if (!is_power(denominator, base)) continue;
         (*result_size)++;
         result[(*result_size) - 1] = dnumber;
@@ -74,7 +72,8 @@ double * final_representation_in_base (size_t * result_size, const int base, int
 
 int main(int argc, char * argv[]) {
     size_t final_rep_size = 0;
-    double * final_rep = final_representation_in_base(&final_rep_size, 8, 5, 0.00001, 0.0002, 0.125, 0.25, 0.5);
+    const double EPS = 1e-10;
+    double * final_rep = final_representation_in_base(EPS, &final_rep_size, 8, 5, 0.00001, 0.0002, 0.125, 0.25, 0.5);
     if (final_rep) {
         if (final_rep_size) {
             for (size_t i = 0; i < final_rep_size; i++) {
