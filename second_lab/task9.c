a44923cb27ae171d716ae09d60c7c44c73884740
scartissue1 <82233682+scartissue1@users.noreply.github.com>
2023-10-20 04:00:58 +0300

    Update task9.c

--- second_lab/task9.c
+++ second_lab/task9.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <ctype.h>
 
-
+#define EPS 1e-10
 
 size_t _strlen(char * string) {
     size_t size = 0;
@@ -12,17 +12,23 @@ size_t _strlen(char * string) {
     return size - 1;
 }
 
-int fraction_validation(char * number) {
-    size_t size = _strlen(number);
-    if (size < 3) return 0;
-    if (number[0] != '0' || number[1] != '.') return 0;
-    for (int i = 2; i < size; i++) {
-        if (!isdigit(number[i])) return 0;
+int double_length(double number) {
+    double tmp = number;
+    int number_size = 0;
+    while (fabs((int)tmp - tmp) > EPS) {
+        tmp *= 10;
+        number_size++;
     }
-    return 1;
+    return number_size;
+    
 }
 
-int is_power(int number, const int base) {
+int is_power(int number, int base) {
+    if (number < base) {
+        int tmp = base;
+        base = number;
+        number = tmp;
+    }
     while (number > 1) {
         if (number % base != 0) return 0;
         number /= base;
@@ -34,10 +40,12 @@ int greatest_common_divisor(int first, int second) {
     return second ? greatest_common_divisor(second, first % second) : first;
 }
 
-int common_fraction(char * number, int * numerator, int * denominator) {
-    size_t num_size = _strlen(number);
-    for (size_t i = 2; i < num_size; i++) {
-        *numerator = *numerator * 10 + (number[i] - '0');
+void common_fraction(double number, int * numerator, int * denominator) {
+    int size = double_length(number);
+    int power = 1;
+    for (int i = 0; i < size; i++) {
+        power *= 10;
+        *numerator = *numerator * 10 + ((int)(number * power) % 10);
         *denominator *= 10;
     }
     int gsc = greatest_common_divisor(*numerator, *denominator);
@@ -45,34 +53,20 @@ int common_fraction(char * number, int * numerator, int * denominator) {
     *denominator /= gsc;
 }
 
-char ** final_representation_in_base (size_t * result_size, const int base, int quantity, ...) {
+double * final_representation_in_base (size_t * result_size, const int base, int quantity, ...) {
     va_list fractions;
     va_start(fractions, quantity);
     *result_size = 0;
-    size_t result_capacity = 2;
-    char ** result = (char **)malloc(sizeof(char *) * result_capacity);
+    double * result = (double *)malloc(sizeof(double) * quantity);
     for (int i = 0; i < quantity; i++) {
         int numerator = 0;
         int denominator = 1;
-        char * number = va_arg(fractions, char *);
-        if (!fraction_validation(number)) {
-            printf("Wrong number\n");
-            return NULL;
-        }
-        common_fraction(number, &numerator, &denominator);
+        double dnumber = va_arg(fractions, double);
+        if (dnumber > 1 - EPS || dnumber < EPS) return NULL;
+        common_fraction(dnumber, &numerator, &denominator);
         if (!is_power(denominator, base)) continue;
         (*result_size)++;
-        result[(*result_size) - 1] = number;
-        if ((*result_size) >= result_capacity) {
-            result_capacity *= 2;
-            char ** tmp = (char **)realloc(result, result_capacity);
-            if (!tmp) {
-                free(result);
-                printf("No memory\n");
-                return NULL;
-            }
-            result = tmp;
-        }
+        result[(*result_size) - 1] = dnumber;
     }
     va_end(fractions);
     return result;
@@ -80,12 +74,16 @@ char ** final_representation_in_base (size_t * result_size, const int base, int 
 
 int main(int argc, char * argv[]) {
     size_t final_rep_size = 0;
-    char ** final_rep = final_representation_in_base(&final_rep_size, 2, 5, "0.00001", "0.0002", "0.125", "0.25", "0.5");
+    double * final_rep = final_representation_in_base(&final_rep_size, 8, 5, 0.00001, 0.0002, 0.125, 0.25, 0.5);
     if (final_rep) {
-        for (size_t i = 0; i < final_rep_size; i++) {
-            printf("%s\n", final_rep[i]);
+        if (final_rep_size) {
+            for (size_t i = 0; i < final_rep_size; i++) {
+                printf("%lf\n", final_rep[i]);
+            }
         }
+        else printf("No such number\n");
         free(final_rep);
     }
+    
     return 0;
 }
